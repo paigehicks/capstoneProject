9e44c4c86ee4a5c27c0c13ab9ce000be7c296e02
Paige Hicks <pnhicks1@gmail.com>
2020-10-27 12:12:09 -0400

    add body commissions

--- components/views/Commissions.js
+++ components/views/Commissions.js
@@ -1,11 +1,29 @@
 export default () => `
-<h1 id="Commissions">Commisions!</h1>
 
-<p class="split-para"> Will do:
-  <ul>
+<div class="mainBody">
 
+  <h1 id="Commissions">Commisions!</h1>
+    <div>
+      <p>Inquires for commission pricing or specific questions can be sent to
+      jennifermeda@live.com</p>
+    </div>
 
+  <p class="split-para"> Will do:
+    <ul>
+      <li> -Pets/Animals </li>
+      <li> -Creatures </li>
+      <li> -Monsters </li>
+      <li> -Icons </li>
+      <li> -Chibis </li>
+      <li> -Furries </li>
+    </ul>
 
-<span>This text is right.</span></p>
-
+  <span> Will <b><u>NOT</u></b> do:
+    <ul>
+      <li> NSFW </li>
+      <li> Mecha/Robots </li>
+      <li> Large Nature Backgrounds </li>
+    </ul>
+  </span></p>
+</div>
 `;
