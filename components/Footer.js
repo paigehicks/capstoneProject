974b794ca87971826cf6478c13fbd5f9d1139aef
Paige Hicks <pnhicks1@gmail.com>
2020-11-07 13:45:17 -0500

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,9 @@
-export default () => ``;
+export default () => `
+<footer class ="footer">
+<a href="https://savvycoders.com">
+&copy; 2020 Savvy Coders Aug 2020 Cohort
+</a>
+<br>
+Paige Hicks Full Stack Web Developer
+</footer>
+`;
--- components/views/Stickers.js
+++ components/views/Stickers.js
@@ -1,31 +1,70 @@
 export default () => `
-<div class="stickerStore">
-<div class="cart">
-<a href="./Cart><img src="https://github.com/paigehicks/capstoneProject/blob/master/Pictures/cart.png?raw=true"></a>
-</div>
-  <h1 class="stickers">Sticker Store!</h1>
-    <div class="stickerGridContainer">
-    <div class="sticker1">
-      <img src="https://github.com/paigehicks/capstoneProject/blob/master/Pictures/letMeSleep.png?raw=true" alt="letMeSleep" style="width:100%">
-      <h1>Let Me Sleep</h1>
-      <p class="price">$5.00</p>
-      <p>Some text about the stickers.</p>
-      <p><button>Add to Cart</button></p>
-      </div>
-    <div class="sticker2">
-      <img src="https://github.com/paigehicks/capstoneProject/blob/master/Pictures/workInProgress.png?raw=true" alt="workInProgress" style="width:100%">
-      <h1>Work in Progress</h1>
-      <p class="price">$5.00</p>
-      <p>Some text about the stickers.</p>
-      <p><button>Add to Cart</button></p>
+<section class="container content-section">
+    <h2 class="section-header">Shop</h2>
+    <div class="shop-items">
+        <div class="shop-item">
+            <span class="shop-item-title">'Let Me Sleep' Sticker</span>
+            <img class="shop-item-image" src="https://github.com/paigehicks/capstoneProject/blob/master/Pictures/letMeSleep.png?raw=true">
+            <div class="shop-item-details">
+                <span class="shop-item-price">$5.00</span>
+                <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+            </div>
+        </div>
+        <div class="shop-item">
+            <span class="shop-item-title">'Work in Progress' Sticker</span>
+            <img class="shop-item-image" src="https://github.com/paigehicks/capstoneProject/blob/master/Pictures/workInProgress.png?raw=true">
+            <div class="shop-item-details">
+                <span class="shop-item-price">$5.00</span>
+                <button class="btn btn-primary shop-item-button"type="button">ADD TO CART</button>
+            </div>
+        </div>
+        <div class="shop-item">
+            <span class="shop-item-title">'Atlas Moth' Sticker</span>
+            <img class="shop-item-image" src="https://github.com/paigehicks/capstoneProject/blob/master/Pictures/atlasMoth.png?raw=true">
+            <div class="shop-item-details">
+                <span class="shop-item-price">$5.00</span>
+                <button class="btn btn-primary shop-item-button" type="button">ADD TO CART</button>
+            </div>
+        </div>
     </div>
-    <div class="sticker3">
-      <img src="https://github.com/paigehicks/capstoneProject/blob/master/Pictures/atlasMoth.png?raw=true" alt="atlasMoth" style="width:100%">
-      <h1>Atlas Moth</h1>
-      <p class="price">$5.00</p>
-      <p>Some text about the stickers.</p>
-    <p><button>Add to Cart</button></p>
+</section>
+
+
+<section class="container content-section">
+    <div class="shop-items">
     </div>
-  </div>
-</div>
+</section>
+
+
+<section class="container content-section">
+    <h2 class="section-header">CART</h2>
+    <div class="cart-row">
+        <span class="cart-item cart-header cart-column">ITEM</span>
+        <span class="cart-price cart-header cart-column">PRICE</span>
+        <span class="cart-quantity cart-header cart-column">QUANTITY</span>
+    </div>
+    <div class="cart-items">
+        <div class="cart-row">
+            <div class="cart-item cart-column">
+                <img class="cart-item-image" src="Images/Shirt.png" width="100" height="100">
+                <span class="cart-item-title">T-Shirt</span>
+        </div>
+        <div class="cart-row">
+            <div class="cart-item cart-column">
+                <img class="cart-item-image" src="Images/Album 3.png" width="100" height="100">
+                <span class="cart-item-title">Album 3</span>
+            </div>
+            <span class="cart-price cart-column">$9.99</span>
+            <div class="cart-quantity cart-column">
+                <input class="cart-quantity-input" type="number" value="2">
+                <button class="btn btn-danger" type="button">REMOVE</button>
+            </div>
+        </div>
+    </div>
+    <div class="cart-total">
+        <strong class="cart-total-title">Total</strong>
+        <span class="cart-total-price">$39.97</span>
+    </div>
+    <button class="btn btn-primary btn-purchase" type="button">PURCHASE</button>
+</section>
 `;
